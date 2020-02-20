633519d5912d40a64e75e903f465c6f04370fad2
Ola Ajibode <ola.ajibode@gmail.com>
2020-02-20 12:03:11 +0000

    Create Counta.scala
    Using basic Scala - no streams yet, accept user inputs and return only the top N items.

---
+++ Counta.scala
@@ -0,0 +1,21 @@
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn.{readInt, readLine}
+
+object Counta extends App {
+
+  println("Enter numbers continuously by pressing Return carriage")
+  var collectedNums = new ListBuffer[Int]()
+  var grab = readLine()
+  collectedNums += grab.toInt
+
+  while (true) {
+    grab = readLine()
+    collectedNums += grab.toInt
+    println(collectedNums)
+    println(collectedNums.distinct.sortWith(_ > _).take(4))
+
+  }
+
+  println(collectedNums)
+}

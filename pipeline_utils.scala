cfc71e9a68952de44e0966710cc000c0ecf48dd9
Ola Ajibode <ola.ajibode@gmail.com>
2020-04-07 17:38:31 +0100

    Create pipeline_utils.scala

---
+++ pipeline_utils.scala
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat 
+import java.util.{Calendar, Date}
+
+object DateUtil {
+/** Date utility for manipulating/generating dashboard components as required **/
+
+private val dateFmt = "yyyy-MM-dd"
+
+def today(): String = {
+val date = new Date
+val sdf = new SimpleDateFormat(dateFmt) sdf.format(date)
+}
+def yesterday(): String = {
+val calender = Calendar.getInstance() calender.roll(Calendar.DAY_OF_YEAR, -1) val sdf = new SimpleDateFormat(dateFmt) sdf.format(calender.getTime())
+}
+def daysAgo(days: Int): String = {
+val calender = Calendar.getInstance() calender.roll(Calendar.DAY_OF_YEAR, -days) val sdf = new SimpleDateFormat(dateFmt) sdf.format(calender.getTime())
+} }
